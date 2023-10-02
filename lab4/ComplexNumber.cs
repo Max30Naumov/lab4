9961020f5335de748739f685564fed62e6c299aa
Max30Naumov <101650471+Max30Naumov@users.noreply.github.com>
2023-10-02 22:01:09 +0300

    Add project files.

---
+++ lab4/ComplexNumber.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    public class ComplexNumber
+    {
+        public double Module { get; private set; } 
+        public double Argument { get; private set; }
+        public double RealPart { get; private set; }
+        public double ImaginaryPart { get; private set; }
+
+        public ComplexNumber()
+        {
+        }
+        public ComplexNumber(double _module, double _argument)
+        {
+            Module = _module;
+            Argument = _argument;
+            RealPart = Module * Math.Cos(Argument);
+            ImaginaryPart = Module * Math.Sin(Argument);
+        }    
+
+        public ComplexNumber Power(double power)
+        {
+            double newModule = Math.Pow(Module, power);
+            double newArgument = Argument * power;
+
+            return new ComplexNumber(newModule, newArgument);
+        }
+        public static ComplexNumber operator +(ComplexNumber firstNumber, ComplexNumber secondNumber)
+        {
+            return new ComplexNumber
+            {
+                RealPart = firstNumber.RealPart + secondNumber.RealPart,
+                ImaginaryPart = firstNumber.ImaginaryPart + secondNumber.ImaginaryPart
+            };
+        }
+
+        public static ComplexNumber operator -(ComplexNumber firstNumber, ComplexNumber secondNumber)
+        {
+            return new ComplexNumber
+            {
+                RealPart = firstNumber.RealPart - secondNumber.RealPart,
+                ImaginaryPart = firstNumber.ImaginaryPart - secondNumber.ImaginaryPart
+            };
+        }
+
+        public static ComplexNumber operator *(ComplexNumber firstNumber, ComplexNumber secondNumber)
+        {
+            return new ComplexNumber
+            {
+                RealPart = firstNumber.RealPart * secondNumber.RealPart - firstNumber.ImaginaryPart * secondNumber.ImaginaryPart,
+                ImaginaryPart = firstNumber.RealPart * secondNumber.ImaginaryPart + firstNumber.ImaginaryPart * secondNumber.RealPart
+            };
+        }
+
+        public static ComplexNumber operator /(ComplexNumber firstNumber, ComplexNumber secondNumber)
+        {
+            double denominator = secondNumber.RealPart * secondNumber.RealPart + secondNumber.ImaginaryPart * secondNumber.ImaginaryPart;
+            return new ComplexNumber
+            {
+                RealPart = (firstNumber.RealPart * secondNumber.RealPart + firstNumber.ImaginaryPart * secondNumber.ImaginaryPart) / denominator,
+                ImaginaryPart = (firstNumber.ImaginaryPart * secondNumber.RealPart - firstNumber.RealPart * secondNumber.ImaginaryPart) / denominator
+            };
+        }
+        public string PrintComplexNumber()
+        {
+            Module = Math.Sqrt(RealPart * RealPart + ImaginaryPart * ImaginaryPart);
+            Argument = Math.Atan2(ImaginaryPart, RealPart);
+
+            string trigonometricFormat = $"R=|{Module}|*(Cos({Argument})+Sin({Argument})i)";
+            string algebraicFormat = $"R={RealPart}+{ImaginaryPart}i";
+
+            return $"Комплексное число в тригонометрическом виде:\n{trigonometricFormat}\n\nКомплексное число в алгебраическом виде:\n{algebraicFormat}";
+
+        }
+
+    }
+}
---
+++ lab4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexNumber a = new ComplexNumber(1, 2);
+            ComplexNumber b = new ComplexNumber(1, 1);
+            ComplexNumber c = new ComplexNumber(3, 4);
+            ComplexNumber d = new ComplexNumber(5, 1);
+            ComplexNumber R = new ComplexNumber();       
+            R = b.Power(3) + ((a+b) / (c-a)) *d;
+            Console.WriteLine(R.PrintComplexNumber());
+        }
+    }
+}
